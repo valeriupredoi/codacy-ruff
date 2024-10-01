26cd13b2682f1c2c16ec963543dab6e06dcdcc5f
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-10-01 16:56:22 +0100

    fix tests

--- src/codacy_ruff_test.py
+++ src/codacy_ruff_test.py
@@ -1,16 +1,18 @@
-from codacy_prospector import *
-import unittest
 import tempfile
+import numpy as np
 
-def withConfigAndSources(config, sources):
+from codacy_ruff import (
+    toJson, chunks, run_tool, getTimeout,
+    DEFAULT_TIMEOUT, Result
+)
+
+
+def assemble_sources(sources):
     with tempfile.TemporaryDirectory() as directory:
-        codacyrcPath = directory + "/.codacyrc"
-        with open(codacyrcPath, "w") as codacyrc:
-            print(config, file=codacyrc)
         for (name, source) in sources:
-            with open(directory + '/' + name, 'w') as f:
+            with open(directory + '/' + name, 'w', encoding='utf-8') as f:
                 print(source, file=f)
-        return runTool(codacyrcPath, directory)
+        return run_tool(directory)
 
 def python3_file():
     source ='''
@@ -37,61 +39,32 @@ def dup_function():
     return 2
 '''
     sources = [('E0102.py', source)]
-    config = '{"tools":[{"name":"prospector"}],"files":["E0102.py"]}'
-    return config, sources
+    return sources
 
-class ResultTest(unittest.TestCase):
-    def test_toJson(self):
-        result = Result("file.py", "message", "id", 80)
-        res = toJson(result)
-        expected = '{"filename": "file.py", "message": "message", "patternId": "id", "line": 80}'
-        self.assertEqual(res, expected)
+def test_toJson():
+    result = Result("file.py", "message", "id", 80)
+    res = toJson(result)
+    expected = '{"filename": "file.py", "message": "message", "patternId": "id", "line": 80}'
+    np.testing.assert_equal(res, expected)
 
-class ProspectorTest(unittest.TestCase):
-    maxDiff = None
-    def test_chunks(self):
-        l = ["file1", "file2"]
-        expected = [["file1", "file2"]]
-        out = chunks(l, 10)
-        self.assertEqual(out, expected)
 
-        expected2 = [["file1"], ["file2"]]
-        out2 = chunks(l, 1)
-        self.assertEqual(expected2, out2)
+def test_chunks():
+    files = ["file1", "file2"]
+    expected = [["file1", "file2"]]
+    out = chunks(files, 10)
+    np.testing.assert_equal(out, expected)
 
-    def test_readConfiguration(self):
-        with tempfile.TemporaryDirectory() as directory:
-            codacyrcPath = os.path.join(directory, ".codacyrc")
-            with open(codacyrcPath, "w") as codacyrc:
-                print('{"tools":[{"name":"prospector", "patterns":[{"patternId":"pyroma"}]}],"files":["C0111.py"]}', file=codacyrc)
-            
-            expectedOptions = {'--without-tool=pylint',
-                               '--without-tool=pydocstyle',
-                               '--without-tool=pycodestyle',
-                               '--without-tool=dodgy',
-                               '--without-tool=mccabe',
-                               '--without-tool=pyflakes',
-                               '--without-tool=profile-validator',
-                               '--with-tool=pyroma'}
-            expectedFiles = ['C0111.py']
-            
-            (options, files) = readConfiguration(codacyrcPath, "docs/test")
-            self.assertEqual(expectedOptions, set(options))
-            self.assertEqual(expectedFiles, files)
+    expected2 = [["file1"], ["file2"]]
+    out2 = chunks(files, 1)
+    np.testing.assert_equal(expected2, out2)
 
-    def test_python3_file(self):
-        (config, sources) = python3_file()
-        result = withConfigAndSources(config, sources)
-        self.assertEqual(len(result), 4)
 
-    def test_no_conf(self):
-        (_, sources) = python3_file()
-        result = withConfigAndSources(None, sources)
-        self.assertEqual(len(result), 4)
+def test_python3_file():
+    sources = python3_file()
+    result = assemble_sources(sources)
+    np.testing.assert_equal(len(result), 3)
 
-    def test_timeout(self):
-        self.assertEqual(getTimeout("60"), 60)
-        self.assertEqual(getTimeout("blabla"), DEFAULT_TIMEOUT)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_timeout():
+    np.testing.assert_equal(getTimeout("60"), 60)
+    np.testing.assert_equal(getTimeout("blabla"), DEFAULT_TIMEOUT)
